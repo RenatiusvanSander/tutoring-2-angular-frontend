1c082b2d9c1fe935e832c8480b63be12e9e81612
Remy Meier <remad@web.de>
2025-09-17 14:04:19 +0200

    feature/keycloak-angular-integration: DataService gets an User with BearerToken

---
+++ src/app/data.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { User } from './model/user';
+import { map, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  static apiUrl: String = 'http://localhost:8082/tutoring3/api';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(id: number) : Observable<User> {
+    return this.http.get<User>(DataService.apiUrl + '/users/get-users/by-user-id/' + id)
+    .pipe(
+      map( (data: User) => User.fromHttp(data))
+    );
+  }
+}
--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { User } from '../model/user';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-overview',
@@ -6,6 +9,35 @@ import { Component } from '@angular/core';
   styleUrl: './overview.component.css',
   standalone: false
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit {
+
+  user: User;
+  id : number;
+  dataLoaded = false;
+  message = '';
+
+  constructor(private dataService: DataService) {
+    this.user = new User();
+    this.id = 1;
+  }
+
+  ngOnInit(): void {
+    this.loadData();
+  }
+
+  loadData() {
+    this.message = 'Loading data...';
+    this.dataService.getUser(+this.id)
+    .pipe(map (user => {
+      return user;
+    }))
+    .subscribe(
+      (next: User) => {
+        this.user = next;
+        this.dataLoaded = true;
+        this.message = '';
+      }
+    );
+  }
 
 }
