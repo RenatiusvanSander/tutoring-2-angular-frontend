720f0bf6e0f5abae0471fae74fc12138740b4547
Remy Meier <remad@web.de>
2025-12-22 15:03:35 +0100

    feature/tutoring-appointment: show all appointments

--- src/app/models/tutoring-appointment.ts
+++ src/app/models/tutoring-appointment.ts
@@ -1,12 +1,21 @@
 export class TutoringAppointment {
-    id!: number;
+    tutoringAppointmentNo!: number;
+    tutoringAppointmentUser!: number; 
     tutoringAppointmentDate!: string;
     tutoringAppointmentStartDateTime!: string;
     tutoringAppointmentEndDateTime!: string;
+    serviceContractId!: number;
     isAccomplished!: boolean;
 
     static fromHttp(tutoringAppointment : TutoringAppointment) : TutoringAppointment {
         const newTutoringAppointment = new TutoringAppointment();
+        newTutoringAppointment.tutoringAppointmentNo = tutoringAppointment.tutoringAppointmentNo;
+        newTutoringAppointment.tutoringAppointmentUser = tutoringAppointment.tutoringAppointmentUser;
+        newTutoringAppointment.tutoringAppointmentDate = tutoringAppointment.tutoringAppointmentDate;
+        newTutoringAppointment.tutoringAppointmentStartDateTime = tutoringAppointment.tutoringAppointmentStartDateTime;
+        newTutoringAppointment.tutoringAppointmentEndDateTime = tutoringAppointment.tutoringAppointmentEndDateTime;
+        newTutoringAppointment.serviceContractId = tutoringAppointment.serviceContractId;
+        newTutoringAppointment.isAccomplished = tutoringAppointment.isAccomplished;
 
         return newTutoringAppointment;
     }
--- src/app/overview/tutoring-appointments/tutoring-appointments.component.ts
+++ src/app/overview/tutoring-appointments/tutoring-appointments.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+import { User } from '../../models/user';
+import { ServiceContract } from '../../models/service-contract';
+import { TutoringAppointment } from '../../models/tutoring-appointment';
+import { ServiceContractDataService } from '../../services/data-services/service-contract-data.service';
+import { TutoringAppointmentDataService } from '../../services/data-services/tutoring-appointment-data.service';
+import { ServiceContractPriceDataService } from '../../services/data-services/service-contract-price-data.service';
+import { ServiceContractPrice } from '../../models/service-contract-price';
 
 @Component({
   selector: 'app-tutoring-appointments',
@@ -7,6 +15,51 @@ import { Component } from '@angular/core';
   templateUrl: './tutoring-appointments.component.html',
   styleUrl: './tutoring-appointments.component.css'
 })
-export class TutoringAppointmentsComponent {
+export class TutoringAppointmentsComponent implements OnInit{
+
+  user!: User;
+  serviceContractPrices!: Array<ServiceContractPrice>;
+  serviceContracts!: Array<ServiceContract>;
+  appointments!: Array<TutoringAppointment>;
+
+  constructor(private dataService: DataService, private serviceContractService: ServiceContractDataService, private serviceContractPriceService: ServiceContractPriceDataService, private appointmentService: TutoringAppointmentDataService) {
+    this.user = new User();
+    this.serviceContractPrices = new Array<ServiceContractPrice>();
+    this.serviceContracts = new Array<ServiceContract>();
+    this.appointments = new Array<TutoringAppointment>();
+  }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      this.user = await this.dataService.getUser();
+
+    } catch(error) {
+      console.error('Failed to load user for add-tutoring-appointment: ', error);
+    }
+    
+    try {
+      this.serviceContractPrices = await this.serviceContractPriceService.getServiceContractPricesByUserId(this.user.userId);
+    } catch(error) {
+      console.error('Failed to load ServiceContractPrices: ', error);
+    }
+
+    if(this.serviceContractPrices.length > 0) {
+      const serviceContractIds: Array<number> = this.serviceContractPrices.map(scp => scp.serviceContractId);
+
+      try {
+        this.serviceContracts = await this.serviceContractService.getServiceContractsByIds(serviceContractIds);
+      } catch(error) {
+        console.error('Failed to load ServiceContracts: ', error);
+      }
+    }
+
+    if(this.serviceContracts.length > 0) {
+      try {
+        // empty for now
+      } catch(error) {
+        // empty for now
+      }
+    }
+  }
 
 }
