7f3bf3b8169e48434205b5bef734785388d4fe8d
R. Meier <remad@web.de>
2025-12-24 19:21:21 +0100

    feature/update-tutoring-appointment : update tutoring appointment

---
+++ src/app/overview/tutoring-appointments/update-tutoring-appointments/update-tutoring-appointments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-tutoring-appointments',
+  standalone: false,
+  
+  templateUrl: './update-tutoring-appointments.component.html',
+  styleUrl: './update-tutoring-appointments.component.css'
+})
+export class UpdateTutoringAppointmentsComponent {
+
+}
